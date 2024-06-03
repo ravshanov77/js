a2d19e91d461f62cd1c09c0801cd14755b04f8e3
Farrukh <farruxlynx@gmail.com>
2024-06-03 17:09:11 +0500

    Initial commit

--- static/scripts/todolist.js
+++ static/scripts/todolist.js
@@ -1,42 +1,45 @@
-const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
-    name: 'make dinner',
-    dueDate: '2022-12-22'
-  }, {
-    name: 'wash dishes',
-    dueDate: '2022-12-22'
-  }];
-render()
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
+render();
 
 function render() {
-    let todoListHTML = ''
+  let todoListHTML = "";
 
-    for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i]
-        const { name, dueDate } = todo
-        const html = `
-        <div>${name}</div>
-        <div>${dueDate}</div>
-        <button class="delete-button" 
-            onclick="todoList.splice(${i}, 1);
-            render(); saveToStorage();">Delete</button>`
-        todoListHTML += html
-    }
-    document.querySelector('.todo-list').innerHTML = todoListHTML
+  todoList.forEach(function (todo, i) {
+    const { name, dueDate } = todo;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-button" 
+          onclick="todoList.splice(${i}, 1);
+          render(); saveToStorage();">Delete</button>`;
+    todoListHTML += html;
+  });
+
+  document.querySelector(".todo-list").innerHTML = todoListHTML;
 }
 
 function add() {
-    const todoName = document.querySelector('.todo-name').value;
-    const todoDate = document.querySelector('.todo-date').value;
+  const todoName = document.querySelector(".todo-name").value;
+  const todoDate = document.querySelector(".todo-date").value;
 
-    todoList.push({name: todoName, dueDate: todoDate});
-    
-    document.querySelector('.todo-name').value = '';
-    document.querySelector('.todo-date').value = '';
+  todoList.push({ name: todoName, dueDate: todoDate });
 
-    render();
-    saveToStorage();
+  document.querySelector(".todo-name").value = "";
+  document.querySelector(".todo-date").value = "";
+
+  render();
+  saveToStorage();
 }
 
 function saveToStorage() {
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
