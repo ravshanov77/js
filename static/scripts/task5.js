c30612a5b0aa81bed9f0a59a50a681c9a05bbcf5
Farrukh <farruxlynx@gmail.com>
2024-06-06 07:23:25 +0500

    Initial commit

--- static/scripts/task5.js
+++ static/scripts/task5.js
@@ -1,6 +1,27 @@
+// let timeoutId;
+
+// function add() {
+//   document.querySelector(".added").innerHTML = "Added";
+//   clearTimeout(timeoutId);
+//   timeoutId = setTimeout(function () {
+//     document.querySelector(".added").innerHTML = "";
+//   }, 2000);
+// }
+
 function add() {
-  document.querySelector(".added").innerHTML = "Added";
-  setTimeout(function () {
-    document.querySelector(".added").innerHTML = "";
-  }, 2000);
+  msg++;
 }
+
+function remove() {
+  msg--;
+}
+
+let msg = 7;
+
+setInterval(function () {
+  if (document.title === "App") {
+    document.title = `(${msg}) New messages`;
+  } else {
+    document.title = "App";
+  }
+}, 1000);
