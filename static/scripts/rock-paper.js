41f8e1fb689b485f8804133427848cbcb9b571c6
Farrukh <farruxlynx@gmail.com>
2024-06-07 07:34:12 +0500

    Initial commit

--- static/scripts/rock-paper.js
+++ static/scripts/rock-paper.js
@@ -7,6 +7,22 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 document.querySelector(".rock-button").addEventListener("click", () => {
   playGame("rock");
 });
+document.querySelector(".paper-button").addEventListener("click", () => {
+  playGame("paper");
+});
+document.querySelector(".scissors-button").addEventListener("click", () => {
+  playGame("scissors");
+});
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    playGame("rock");
+  } else if (event.key === "p") {
+    playGame("paper");
+  } else if (event.key === "s") {
+    playGame("scissors");
+  }
+});
 
 update();
 
