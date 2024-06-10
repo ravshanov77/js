948e8cea544c4b0161aec8f2bfaedb09a6762ef4
Farrukh <farruxlynx@gmail.com>
2024-06-10 18:23:26 +0500

    Initial commit

--- static/scripts/task6.js
+++ static/scripts/task6.js
@@ -13,4 +13,17 @@ function countPos(arr) {
   return sum;
 }
 
-console.log(countPos([-1, 2, -3, 7, -1]));
+// console.log(countPos([-1, 2, -3, 7, -1]));
+// console.log(countPos([1, 2, -3, 7, -1, 9, 0, 8]));
+
+function addNum(arr, num) {
+  return arr.map((value) => value + num);
+}
+
+// console.log(addNum([6, 6, 6, 6], 1));
+
+function removeEgg(food) {
+  return food.filter((food) => food !== "egg");
+}
+
+// console.log(removeEgg(["egg", "apple"]));
